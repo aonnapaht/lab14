a41645d6ac594bb445f37f10e158c8ec15c64bfb
aonnapaht <napaht_d@cmu.ac.th>
2023-01-13 17:48:28 +0700

    napaht

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,41 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double a[],int N,double b[]){
+    
+    double sum = 0;
+    for(int i = 0;i < N;i++){
+        sum += a[i];     
+    }
+    b[0] = sum/N;
+    
+    
+    double x = 0;
+    for(int i = 0;i < N;i++){
+        x += pow(a[i],2);
+    }
+    b[1] = sqrt((x/N) - pow(b[0],2));
+    
+    
+    double g = 1;
+    for(int i = 0;i < N;i++){
+        g *= a[i];
+    }
+    b[2] = pow(g,pow(N,-1));
+    
+    
+    double h = 0;
+    for(int i = 0;i < N;i++){
+        h += 1/a[i];
+    }
+    b[3] = N/h;
+    
+    
+    b[4] = a[0] , b[5] = a[0];
+    for(int i = 1;i < N;i++){
+        if(a[i] > b[4]) b[4] = a[i];
+        if(a[i] < b[5]) b[5] = a[i]; 
+    }
+    
+}
